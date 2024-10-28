4c95e9ff176929825c9f6d2e8b907b9764e7e5ec
Jimwel Valdez <jimvdz@protonmail.com>
2024-10-28 17:29:58 +0800

    Add initial customer data

--- public/js/customer-data.js
+++ public/js/customer-data.js
@@ -1,3 +1,19 @@
-const customers = [];
+const customers = [
+    {
+        "id": 1, "name": "Kate Anne David", "email": "K8Dvd@kumachicoffee.ph", "num": "09201234567", "preferred": "Social Media Marketing"
+    },
+    {
+        "id": 2, "name": "Jimwel Valdez", "email": "jimvdz@kumachicoffee.ph", "num": "09211234567", "preferred": "Email Marketing"
+    },
+    {
+        "id": 3, "name": "Chester Jonathan Tayag", "email": "CJT22@kumachicoffee.ph", "num": "09221234567", "preferred": "Social Media Marketing"
+    },
+    {
+        "id": 4, "name": "Christine Jasline Natividad", "email": "itsjayceee@kumachicoffee.ph", "num": "09231234567", "preferred": "Email Marketing"
+    },
+    {
+        "id": 5, "name": "Luis Miguel Cayanan", "email": "lmcay@kumachicoffee.ph", "num": "09241234567", "preferred": "Social Media Marketing"
+    }
+];
 
 module.exports = customers;
--- public/js/messages-data.js
+++ public/js/messages-data.js
@@ -1,3 +1,19 @@
-const messages = [];
+const messages = [
+    {
+        "id": 1, "name": "Kate Anne David", "email": "K8Dvd@kumachicoffee.ph", "num": "09201234567", "subject": "Amazing Atmosphere", "message": "I absolutely loved the cozy ambiance of the shop! It made my visit so enjoyable. I can't wait to come back.", "preferred": "Social Media Marketing"
+    },
+    {
+        "id": 2, "name": "Jimwel Valdez", "email": "jimvdz@kumachicoffee.ph", "num": "09211234567", "subject": "Great Coffee Experience", "message": "I loved the rich flavor of your coffee! The atmosphere was cozy, and the staff was friendly. Keep up the great work.", "preferred": "Email Marketing"
+    },
+    {
+        "id": 3, "name": "Chester Jonathan Tayag", "email": "CJT22@kumachicoffee.ph", "num": "09221234567", "subject": "A Perfect Place to Relax", "message": "This coffee shop is the perfect spot to unwind. The comfortable seating and peaceful environment make it a go-to for relaxation.", "preferred": "Social Media Marketing"
+    },
+    {
+        "id": 4, "name": "Christine Jasline Natividad", "email": "itsjayceee@kumachicoffee.ph", "num": "09231234567", "subject": "Delicious Treats", "message": "The pastries were absolutely delightful! Paired with a perfectly brewed cup, it made for an amazing experience. I'll be back for more.", "preferred": "Email Marketing"
+    },
+    {
+        "id": 5, "name": "Luis Miguel Cayanan", "email": "lmcay@kumachicoffee.ph", "num": "09241234567", "subject": "Great Flavors", "message": "The flavors of both the coffee and pastries were outstanding! I loved every bite and sip. Can't wait to return for another round.", "preferred": "Social Media Marketing"
+    }
+];
 
 module.exports = messages;
